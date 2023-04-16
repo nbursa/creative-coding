268d7a9fd892aa0533f1654ba7eb5e14a972db62
Nenad Bursać <nbursa@gmail.com>
2023-04-16 14:49:24 +0200

    Chatgpt init

--- src/components/chatgpt/config.ts
+++ src/components/chatgpt/config.ts
@@ -1,5 +1,4 @@
 export const apiConfig = {
-  apiKey: "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",
-  apiUrl: "https://api.openai.com/v1/chat/completion",
-  apiCompletitionsUrl: "https://api.openai.com/v1/engines/davinci-codex/completions"
+  apiKey: process.env.CHATGPT_API_KEY,
+  apiUrl: process.env.CHATGPT_API_URL,
 };
