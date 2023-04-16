d3ab694fbd16df4864f4a8eefe207a2ba26801e2
Nenad Bursać <nbursa@gmail.com>
2023-04-16 16:14:45 +0200

    Chatgpt gpt model

--- src/components/chatgpt/ChatGPTInputForm.tsx
+++ src/components/chatgpt/ChatGPTInputForm.tsx
@@ -15,6 +15,7 @@ const openai = new OpenAIApi(configuration);
 const ChatGPTInputForm: React.FC = () => {
   const [input, setInput] = useState('');
   const [conversation, setConversation] = useState<ConversationItem[]>([]);
+  const [loading, setLoading] = useState(false);
   const messagesRef = useRef<HTMLDivElement>(null);
   const inputRef = useRef<HTMLInputElement>(null);
 
@@ -29,14 +30,25 @@ const ChatGPTInputForm: React.FC = () => {
     const messagesElement = messagesRef.current;
 
     if (messagesElement) {
-      messagesElement.scrollTop = messagesElement.scrollHeight;
+      const lastMessageIndex = conversation.length - 1;
+      const lastMessageElement = messagesElement.children[lastMessageIndex];
+
+      if (lastMessageElement) {
+        const lastMessageHeight = lastMessageElement.getBoundingClientRect().height;
+        messagesElement.scrollTop = messagesElement.scrollHeight - lastMessageHeight;
+      } else {
+        messagesElement.scrollTop = messagesElement.scrollHeight;
+      }
     }
   }, [conversation]);
+
 
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
 
     if (!input.trim()) return;
+
+    setLoading(true);
 
     let prompt = "Create a personalized ChatGPT model that imitates my writing style, interests, and communication preferences. Provide tips on how to effectively build and develop full stack applications using nodejs, javascript and css. Provide any code from response wrapped in triple backticks (```). Your name is Nenad. \nUser: Who are you?\nAI: My name is Nenad, I am frontend developer from Belgrade, Serbia with 8 years of professional development experience working with popular frontend frameworks and libraries";
 
@@ -52,7 +64,7 @@ const ChatGPTInputForm: React.FC = () => {
           {role: "user", content: finalPrompt},
         ],
         max_tokens: 1000,
-        temperature: 0.7,
+        temperature: 0.5,
         top_p: 1,
         frequency_penalty: 0.0,
         presence_penalty: 0.0,
@@ -71,6 +83,8 @@ const ChatGPTInputForm: React.FC = () => {
       setInput('');
     } catch (error) {
       console.error(error);
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -84,18 +98,24 @@ const ChatGPTInputForm: React.FC = () => {
     <div className="w-full h-full p-4 justify-between">
       <div ref={messagesRef}
            className="min-h-[60vh] max-h-[60vh] grid grid-cols-1 overflow-hidden overflow-y-auto">
+        {loading && (
+          <div
+            className="absolute top-0 left-0 w-full flex items-center justify-center bg-[var(--background)]">
+            <div className="loader"></div>
+          </div>
+        )}
         {conversation.map((item, index) => (
           <div key={index}
-               className="shrink mb-2 p-2 grid grid-cols-[15px_1fr] grid-rows-1 gap-2 max-w-full min-w-full max-h-min order-last border-t">
-            <div className="">{index + 1}</div>
-            <div className="w-full h-full flex-wrap">
-              <div className="grid gap-1 grid-cols-[50px_1fr] mb-4">
+               className="shrink mb-2 p-2 grid grid-cols-1 grid-rows-1 gap-2 max-w-full min-w-full max-h-min order-last border-t">
+            {/*<div className="text-xs">{index + 1}</div>*/}
+            <div className="w-full h-full flex-wrap text-sm">
+              <div className="grid gap-1 grid-cols-[50px_1fr] mb-4 text-yellow-300">
                 <div className="">Q:</div>
                 <div className="whitespace-pre break-words">
                   <pre className="whitespace-pre-line">{item.userPrompt}</pre>
                 </div>
               </div>
-              <div className="grid gap-1 grid-cols-[50px_1fr]">
+              <div className="grid gap-1 grid-cols-[50px_1fr] text-blue-400">
                 <div className="">AI:</div>
                 <div className="">
                   <pre className="whitespace-pre-line">{item.aiResponse}</pre>
@@ -113,7 +133,7 @@ const ChatGPTInputForm: React.FC = () => {
           onChange={(e) => setInput(e.target.value)}
           onKeyDown={handleKeyDown}
           placeholder="Ask ChatGPT something..."
-          className="w-full p-2 border border-[var(--color-gray)] bg-[var(--color-gray)] rounded-lg focus:outline-none"
+          className="w-full p-2 text-sm border border-[var(--color-gray)] bg-[var(--color-gray)] rounded-lg focus:outline-none"
         />
         <button
           type="submit"
