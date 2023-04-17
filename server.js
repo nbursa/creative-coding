570134752e4e082eac041f511c546b843f5530ad
Nenad Bursać <nbursa@gmail.com>
2023-04-17 15:18:54 +0200

    server

--- server.js
+++ server.js
@@ -1,40 +1,39 @@
 const {createServer} = require('http');
-const next = require('next')
+const next = require('next');
 const {parse} = require('url');
+console.log('process.env.NODE_ENV: ', process.env.NODE_ENV);
 const dev = process.env.NODE_ENV !== 'production';
 const hostname = 'localhost';
-const port = Number(process.env.PORT) || 3001;
+const port = Number(process.env.PORT) || 3003;
 const app = next({dev, hostname, port});
 const handle = app.getRequestHandler();
 
 async function handleRequest(
-    req,
-    res,
-    parsedUrl
+  req,
+  res,
+  parsedUrl
 ) {
-    try {
-        const parsedUrl = parse(req.url, true);
-        const {pathname, query} = parsedUrl;
-
-        if (pathname === '/a') {
-            await app.render(req, res, '/a', query);
-        } else if (pathname === '/b') {
-            await app.render(req, res, '/b', query);
-        } else {
-            await handle(req, res, parsedUrl);
-        }
-    } catch (err) {
-        console.error('Error occurred handling', req.url, err);
-        res.statusCode = 500;
-        res.end('Node - internal server error');
+  try {
+    const parsedUrl = parse(req.url, true);
+    const {pathname, query} = parsedUrl;
+    console.log('pathname: ', pathname);
+    if (pathname) {
+      await app.render(req, res, pathname, query);
+    } else {
+      await handle(req, res, parsedUrl);
     }
+  } catch (err) {
+    console.error('Error occurred handling', req.url, err);
+    res.statusCode = 500;
+    res.end('Node - internal server error');
+  }
 }
 
 app.prepare().then(() => {
-    createServer(handleRequest).listen(port, (err) => {
-        if (err) throw err;
-        console.log(`> Ready on https://${hostname}:${port}`);
-    });
+  createServer(handleRequest).listen(port, (err) => {
+    if (err) throw err;
+    console.log(`> Server is ready on https://${hostname}:${port}`);
+  });
 });
 
 module.exports = {};
