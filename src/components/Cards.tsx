fafb88567dadf4b0d243e5a0a44f30d32afa3620
Nenad Bursać <nbursa@gmail.com>
2023-05-01 14:18:45 +0200

    cards links

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -5,13 +5,13 @@ const Cards: React.FC<CardsProp> = ({title, description, className}) => {
   return (
     <div className={className}>
       {title && <h2>{title}</h2>}
-      <ButtonCard title="Docs" description="Find in-depth information about Next.js features and API." href="/docs"/>
+      <ButtonCard title="Demo" description="Take a look at some demo components." href="/demo"/>
       <ButtonCard title="Games" description="Play games to learn Next.js in an interactive way."
                   href="/games"/>
       <ButtonCard title="AI" description="AI powered chatbot with GPT-3.5"
                   href="/ai"/>
-      <ButtonCard title="Deploy" description="Instantly deploy your Next.js site to a shareable URL with Vercel."
-                  href="/deploy"/>
+      <ButtonCard title="Deploy" description="Instantly deploy your site to a shareable URL."
+                  href="/demo"/>
       {description && <p>{description}</p>}
     </div>
   )
